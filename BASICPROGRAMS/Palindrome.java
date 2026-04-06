8871910fbfa7e1a7b09fc649c3cc38d2afb0224e
Anam Shaikh <anamshaikh7296@gmail.com>
2026-04-06 23:30:13 +0530

    Align GitHub structure with local BASICPROGRAMS layout

--- BASICPROGRAMS/Palindrome.java
+++ BASICPROGRAMS/Palindrome.java
@@ -1,4 +1,4 @@
-class Solution {
+class Palindrome{
     // public boolean isPalindrome(int x) {
         public static void main(String[] args) {
           System.out.println(isPalindrome(121));
--- Variables.java
+++
@@ -1,9 +0,0 @@
-public class Variables {
-  public static void main(String[] args) {
-    int s1 = 10;
-    System.out.println(s1);
-
-    int s2 = 180;
-    System.out.println(s2);
-  }
-}
